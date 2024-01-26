732d39b33c43f7451c7c44725a85653784e0f01a
innajain <m.shreyansh.jain@gmail.com>
2024-01-26 16:17:28 +0530

    changing base url

--- frontend/src/api requests/sendApiRequest.ts
+++ frontend/src/api requests/sendApiRequest.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://live-quiz-app-backend.vercel.app/";
 
 export const sendApiRequest = async (path: string, body: any): Promise<{success: boolean, message? : string, data? : any}> => {
   const response = fetch(`${BASE_URL}${path}`, {
